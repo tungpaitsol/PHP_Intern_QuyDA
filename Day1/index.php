6db3812a991d22d267e2d7fc8b005cb0b1366302
anhquygt102 <anhquygt102@gmail.com>
2019-04-09 09:00:51 +0700

    fix request

--- Day1/index.php
+++ Day1/index.php
@@ -10,22 +10,53 @@
 <body>
 <?php
 $kq = '';
-if(isset($_POST['button'])) {
-    $a = isset($_POST['a']) ? $_POST['a'] : '';
-    $b = isset($_POST['b']) ? $_POST['b'] : '';
-    $c = isset($_POST['c']) ? $_POST['c'] : '';
-    $delta = ($b*$b) - (4*$a*$c);
-    if($a != 0) {
+$error = [];
+$e = [];
+if (isset($_POST['button'])) {
+    $a = isset($_POST['a']) ? ($_POST['a']) : '';
+    $b = isset($_POST['b']) ? ($_POST['b']) : '';
+    $c = isset($_POST['c']) ? ($_POST['c']) : '';
+
+    if (empty($a) && $a != 0) {
+        $error[] = 'Bạn chưa nhập a';
+    }
+
+    if (empty($b) && $b != 0) {
+        $error[] = 'Bạn chưa nhập b';
+    }
+    if (empty($c) && $c != 0) {
+        $error[] = 'Bạn chưa nhập c';
+    }
+    if (!is_numeric($a)) {
+        $e[] = 'a không đúng định dạng';
+    }
+    if (!is_numeric($b)) {
+        $e[] = 'b không đúng định dạng';
+    }
+    if (!is_numeric($c)) {
+        $e[] = 'c không đúng định dạng';
+    }
+
+    if ($error != []) {
+       echo $error[0];
+       die();
+    }
+    if ($e != []) {
+        echo $e[0];
+        die();
+    }
+
+    $delta = ($b * $b) - (4 * $a * $c);
+    if ($a != 0) {
         if ($delta < 0) {
             $kq = 'PT vo nghiem';
         } else if ($delta == 0) {
-            $kq = 'PT co nghiem kep la : x1 = x2 = ' . (-$b / 2 * $a);
+            $kq = 'PT co nghiem kep la : x1 = x2 = ' . (-$b / (2 * $a));
         } else {
             $kq = 'PT co 2 nghiem phan biet la : x1 = ' . ((-$b + sqrt($delta)) / 2 * $a);
             $kq .= ',x2 = ' . ((-$b - sqrt($delta)) / 2 * $a);
         }
-    }
-    else{
+    } else {
         if ($b == 0) {
             if ($c == 0) {
                 $kq = 'Vo so nghiem';
@@ -34,14 +65,15 @@ if(isset($_POST['button'])) {
                 $kq = 'Vo nghiem';
             }
         } else {
-            $kq = 'x= '. -($c/$b);
+            $kq = 'x= ' . -($c / $b);
         }
     }
 }
 ?>
+
 <h1>Phuong Trinh Bac 2</h1>
 <form method="post" action="">
-    <input type="number" name="a">x<sup>2</sup> + <input type="number" name="b">x + <input type="number" name="c"> = 0
+    <input type="text" name="a" value="<?php echo isset($a) ? $a : '' ?>">x<sup>2</sup>+ <input type="text" name="b" value="<?php echo isset($b) ? $b : '' ?>">x + <input type="text" name="c" value="<?php echo isset($c) ? $c : '' ?>"> = 0
     <input type="submit" name="button" value="ket qua"/>
 </form>
 <?php
