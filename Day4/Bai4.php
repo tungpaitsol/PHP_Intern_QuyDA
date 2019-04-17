63a7a5e65e629bb55d8ce1713958d9e480b61277
anhquygt102 <anhquygt102@gmail.com>
2019-04-17 13:41:46 +0700

    bai4

--- Day4/Bai4.php
+++ Day4/Bai4.php
@@ -1,1 +1,214 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bài 4</title>
+</head>
+<body>
+<form method="post" action="">
+    <input type="submit" name="priceup" value="price tăng"/>
+    <input type="submit" name="pricedown" value="price giảm"/>
+    <input type="submit" name="orderup" value="location tăng"/>
+    <input type="submit" name="orderdown" value="location giảm"/>
+    <input type="submit" name="totalup" value="tổng tăng"/>
+    <input type="submit" name="totaldown" value="tổng giảm"/>
+</form>
+
 <?php
+if (isset($_POST['priceup']) ) {
+    clickone();
+}
+
+if (isset($_POST['pricedown']) ) {
+    clicktwo();
+}
+
+if (isset($_POST['orderup']) ) {
+    clickthree();
+}
+
+if (isset($_POST['orderdown']) ) {
+    clickfour();
+}
+
+if (isset($_POST['totalup']) ) {
+    clickfive();
+}
+
+if (isset($_POST['totaldown']) ) {
+    clicksix();
+}
+
+function clickone(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "test";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM demo ORDER BY price ASC;";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<table><tr><th>ID</th><th>Name</th><th>Price</th><th>Quantity</th><th>Location</th></tr>";
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td>".$row["id"]."</td><td>".$row["name"]."</td><td>".$row["price"]."</td><td>".$row["quantity"]."</td><td>".$row["location"]."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
+}
+
+function clicktwo(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "test";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM demo ORDER BY price DESC";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        echo "<table><tr><th>ID</th><th>Name</th><th>Price</th><th>Quantity</th><th>Location</th></tr>";
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td>".$row["id"]."</td><td>".$row["name"]."</td><td>".$row["price"]."</td><td>".$row["quantity"]."</td><td>".$row["location"]."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
+}
+
+function clickthree(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "test";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM demo ORDER BY location ASC";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<table><tr><th>ID</th><th>Name</th><th>Price</th><th>Quantity</th><th>Location</th></tr>";
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td>".$row["id"]."</td><td>".$row["name"]."</td><td>".$row["price"]."</td><td>".$row["quantity"]."</td><td>".$row["location"]."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
+}
+
+function clickfour(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "test";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM demo ORDER BY location DESC";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<table><tr><th>ID</th><th>Name</th><th>Price</th><th>Quantity</th><th>Location</th></tr>";
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td>".$row["id"]."</td><td>".$row["name"]."</td><td>".$row["price"]."</td><td>".$row["quantity"]."</td><td>".$row["location"]."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
+}
+
+function clickfive(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "test";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM demo ORDER BY (quantity * price) ASC";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<table><tr><th>ID</th><th>Name</th><th>Price</th><th>Quantity</th><th>Location</th></tr>";
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td>".$row["id"]."</td><td>".$row["name"]."</td><td>".$row["price"]."</td><td>".$row["quantity"]."</td><td>".$row["location"]."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
+}
+
+function clicksix(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "test";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM demo ORDER BY (quantity * price) DESC";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<table><tr><th>ID</th><th>Name</th><th>Price</th><th>Quantity</th><th>Location</th></tr>";
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td>".$row["id"]."</td><td>".$row["name"]."</td><td>".$row["price"]."</td><td>".$row["quantity"]."</td><td>".$row["location"]."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
+}
+?>
+
+</body>
+</html>
