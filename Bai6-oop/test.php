ddb95bf450bba71886c102b015fc74c83a1edda3
anhquygt102 <anhquygt102@gmail.com>
2019-05-24 11:14:10 +0700

    bai6

---
+++ Bai6-oop/test.php
@@ -0,0 +1,455 @@
+<?php
+
+
+$listMemberFullTime = array(
+    array(
+        'code' => '001',
+        'full_name' => 'Nguyen Van A',
+        'age' => 25,
+        'gender' => 0,
+        'marital_status' => 0,
+        'total_work_time' => 0,
+        'salary' => 15000000,
+        'workdays' => 0,
+        'start_work_time' => '08:00:00',
+        'work_hour' => 8,
+        'has_lunch_break' => 1
+    ),
+    array(
+        'code' => '002',
+        'full_name' => 'Nguyen Thi B',
+        'age' => 30,
+        'gender' => 1,
+        'marital_status' => 1,
+        'total_work_time' => 0,
+        'salary' => 10000000,
+        'workdays' => 0,
+        'start_work_time' => '08:30:00',
+        'work_hour' => 8,
+        'has_lunch_break' => 1
+    ),
+    array(
+        'code' => '003',
+        'full_name' => 'Nguyen Van C',
+        'age' => 28,
+        'gender' => 0,
+        'marital_status' => 1,
+        'total_work_time' => 0,
+        'salary' => 20000000,
+        'workdays' => 0,
+        'start_work_time' => '09:00:00',
+        'work_hour' => 8,
+        'has_lunch_break' => 1
+    )
+);
+
+$listMemberPartTime = array(
+    array(
+        'code' => '004',
+        'full_name' => 'Nguyen Xuan P',
+        'age' => 22,
+        'gender' => 0,
+        'marital_status' => 0,
+        'total_work_time' => 0,
+        'salary' => 4000000,
+        'workdays' => 0,
+        'start_work_time' => '09:00:00',
+        'work_hour' => 4,
+        'has_lunch_break' => 0
+    ),
+    array(
+        'code' => '005',
+        'full_name' => 'Nguyen Ngoc T',
+        'age' => 21,
+        'gender' => 1,
+        'marital_status' => 0,
+        'total_work_time' => 0,
+        'salary' => 3500000,
+        'workdays' => 0,
+        'start_work_time' => '08:30:00',
+        'work_hour' => 3,
+        'has_lunch_break' => 1
+    )
+);
+
+$listWorkTime = array(
+    array(
+        'member_code' => '001',
+        'start_datetime' => '2019-04-01 08:00:00',
+        'end_datetime' => '2019-04-01 19:00:00'
+    ),
+
+    array(
+        'member_code' => '001',
+        'start_datetime' => '2019-05-02 08:10:00',
+        'end_datetime' => '2019-05-02 17:00:00'
+    ),
+    array(
+        'member_code' => '001',
+        'start_datetime' => '2019-04-03 08:50:00',
+        'end_datetime' => '2019-04-03 18:00:00'
+    ),
+    array(
+        'member_code' => '001',
+        'start_datetime' => '2019-04-04 08:20:00',
+        'end_datetime' => '2019-04-04 18:20:00'
+    ),
+    array(
+        'member_code' => '001',
+        'start_datetime' => '2019-04-05 09:02:00',
+        'end_datetime' => '2019-04-05 17:40:00'
+    ),
+    array(
+        'member_code' => '001',
+        'start_datetime' => '2019-04-06 08:20:00',
+        'end_datetime' => '2019-04-06 18:30:00'
+    ),
+    array(
+        'member_code' => '002',
+        'start_datetime' => '2019-04-01 08:00:00',
+        'end_datetime' => '2019-04-01 19:00:00'
+    ),
+    array(
+        'member_code' => '002',
+        'start_datetime' => '2019-05-02 08:10:00',
+        'end_datetime' => '2019-05-02 17:00:00'
+    ),
+    array(
+        'member_code' => '002',
+        'start_datetime' => '2019-04-03 08:50:00',
+        'end_datetime' => '2019-04-03 18:00:00'
+    ),
+    array(
+        'member_code' => '002',
+        'start_datetime' => '2019-04-04 08:20:00',
+        'end_datetime' => '2019-04-04 18:20:00'
+    ),
+    array(
+        'member_code' => '002',
+        'start_datetime' => '2019-04-05 09:02:00',
+        'end_datetime' => '2019-04-05 17:40:00'
+    ),
+    array(
+        'member_code' => '002',
+        'start_datetime' => '2019-04-06 08:20:00',
+        'end_datetime' => '2019-04-06 18:30:00'
+    ),
+
+    array(
+        'member_code' => '003',
+        'start_datetime' => '2019-04-01 08:00:00',
+        'end_datetime' => '2019-04-01 19:00:00'
+    ),
+    array(
+        'member_code' => '003',
+        'start_datetime' => '2019-04-02 08:10:00',
+        'end_datetime' => '2019-04-02 17:00:00'
+    ),
+    array(
+        'member_code' => '003',
+        'start_datetime' => '2019-04-03 08:50:00',
+        'end_datetime' => '2019-04-03 18:00:00'
+    ),
+    array(
+        'member_code' => '003',
+        'start_datetime' => '2019-04-04 08:20:00',
+        'end_datetime' => '2019-04-04 18:20:00'
+    ),
+    array(
+        'member_code' => '003',
+        'start_datetime' => '2019-04-05 09:02:00',
+        'end_datetime' => '2019-04-05 17:40:00'
+    ),
+    array(
+        'member_code' => '003',
+        'start_datetime' => '2019-03-06 08:20:00',
+        'end_datetime' => '2019-03-06 18:30:00'
+    ),
+
+
+    array(
+        'member_code' => '004',
+        'start_datetime' => '2019-04-01 08:00:00',
+        'end_datetime' => '2019-04-01 12:00:00'
+    ),
+    array(
+        'member_code' => '004',
+        'start_datetime' => '2019-04-02 08:10:00',
+        'end_datetime' => '2019-04-02 11:30:00'
+    ),
+    array(
+        'member_code' => '004',
+        'start_datetime' => '2019-04-03 08:50:00',
+        'end_datetime' => '2019-04-03 11:40:00'
+    ),
+    array(
+        'member_code' => '004',
+        'start_datetime' => '2019-04-04 08:20:00',
+        'end_datetime' => '2019-04-04 12:10:00'
+    ),
+    array(
+        'member_code' => '004',
+        'start_datetime' => '2019-04-05 09:02:00',
+        'end_datetime' => '2019-04-05 11:45:00'
+    ),
+    array(
+        'member_code' => '004',
+        'start_datetime' => '2019-04-06 08:20:00',
+        'end_datetime' => '2019-04-06 12:30:00'
+    ),
+
+    array(
+        'member_code' => '005',
+        'start_datetime' => '2019-04-01 08:00:00',
+        'end_datetime' => '2019-04-01 12:00:00'
+    ),
+    array(
+        'member_code' => '005',
+        'start_datetime' => '2019-04-02 08:10:00',
+        'end_datetime' => '2019-04-02 11:30:00'
+    ),
+    array(
+        'member_code' => '005',
+        'start_datetime' => '2019-04-03 08:50:00',
+        'end_datetime' => '2019-04-03 11:40:00'
+    ),
+    array(
+        'member_code' => '005',
+        'start_datetime' => '2019-04-04 08:20:00',
+        'end_datetime' => '2019-04-04 12:10:00'
+    ),
+    array(
+        'member_code' => '005',
+        'start_datetime' => '2019-04-05 09:02:00',
+        'end_datetime' => '2019-04-05 11:45:00'
+    ),
+    array(
+        'member_code' => '005',
+        'start_datetime' => '2019-04-06 08:20:00',
+        'end_datetime' => '2019-04-06 12:30:00'
+    ),
+
+);
+
+$list = [[], [], [], [], [], [], [], [], [], [], [], []];
+
+$day_oof = array(
+    strtotime('2019-01-01'),
+    strtotime('2019-02-04'),
+    strtotime('2019-02-05'),
+    strtotime('2019-02-06'),
+    strtotime('2019-02-07'),
+    strtotime('2019-02-08'),
+    strtotime('2019-04-14'),
+    strtotime('2019-04-30'),
+    strtotime('2019-05-01'),
+    strtotime('2019-09-02'),
+);
+
+$allDayOff = [];
+foreach ($day_oof as $value) {
+    $date = getdate($value);
+    array_push($allDayOff, $date['mon']);
+}
+
+$numberHollyday = [];
+for ($i = 1; $i < 13; $i++) {
+    $dem = 0;
+    foreach ($allDayOff as $value) {
+        if ($i == $value) {
+            $dem++;
+        }
+    }
+    array_push($numberHollyday, $dem);
+}
+
+$workday1 = 31 - 8 - $numberHollyday[0];
+
+$workday2 = 28 - 8 - $numberHollyday[1];
+
+$workday3 = 31 - 8 - $numberHollyday[2];
+
+$workday4 = 30 - 8 - $numberHollyday[3];
+
+$workday5 = 31 - 8 - $numberHollyday[4];
+
+$workday6 = 30 - 8 - $numberHollyday[5];
+
+$workday7 = 31 - 8 - $numberHollyday[6];
+
+$workday8 = 31 - 8 - $numberHollyday[7];
+
+$workday9 = 30 - 8 - $numberHollyday[8];
+
+$workday10 = 31 - 8 - $numberHollyday[9];
+
+$workday11 = 30 - 8 - $numberHollyday[10];
+
+$workday12 = 31 - 8 - $numberHollyday[11];
+
+foreach ($listWorkTime as $item) {
+    $date = getdate(strtotime($item['start_datetime']));
+//    if ($date['mon'] == 1) {
+//        array_push($list[0], $item);
+//    }
+//    if ($date['mon'] == 2) {
+//        array_push($list[1], $item);
+//    }
+//    if ($date['mon'] == 3) {
+//        array_push($list[2], $item);
+//    }
+//    if ($date['mon'] == 4) {
+//        array_push($list[3], $item);
+//    }
+//    if ($date['mon'] == 5) {
+//        array_push($list[4], $item);
+//    }
+//    if ($date['mon'] == 6) {
+//        array_push($list[5], $item);
+//    }
+//    if ($date['mon'] == 7) {
+//        array_push($list[6], $item);
+//    }
+//    if ($date['mon'] == 8) {
+//        array_push($list[7], $item);
+//    }
+//    if ($date['mon'] == 9) {
+//        array_push($list[8], $item);
+//    }
+//    if ($date['mon'] == 10) {
+//        array_push($list[9], $item);
+//    }
+//    if ($date['mon'] == 11) {
+//        array_push($list[10], $item);
+//    }
+//    if ($date['mon'] == 12) {
+//        array_push($list[11], $item);
+//    }
+    for ($i=1; $i <= count($list); $i++){
+        if ($date['mon'] == $i) {
+        array_push($list[$i -1], $item);
+    }
+    }
+
+}
+
+$workdays = [];
+foreach ($listMemberFullTime as $v) {
+    $count = 0;
+    foreach ($listWorkTime as $value) {
+        if ($v['code'] == $value['member_code']) {
+            if ((((strtotime($value['end_datetime']) - strtotime($value['start_datetime'])) / 3600) >= 4) && (((strtotime($value['end_datetime']) - strtotime($value['start_datetime'])) / 3600) < 8)) {
+                $count = $count + 0.5;
+            }
+            if (((strtotime($value['end_datetime']) - strtotime($value['start_datetime'])) / 3600) >= 8) {
+                $count = $count + 1;
+            }
+        }
+    }
+    array_push($workdays, $count);
+}
+
+for ($i = 0; $i < count($listMemberFullTime); $i++) {
+    $listMemberFullTime[$i]['workdays'] = $workdays[$i];
+}
+
+$arrayAllMonth = [];
+foreach ($list as $month) {
+    $people = [];
+    foreach ($listMemberFullTime as $member) {
+        $count = 0;
+        foreach ($month as $array) {
+            if ($array['member_code'] == $member['code']) {
+                if ((((strtotime($array['end_datetime']) - strtotime($array['start_datetime'])) / 3600 - 1.5) >= 4)
+                    && (((strtotime($array['end_datetime']) - strtotime($array['start_datetime'])) / 3600 - 1.5) < 8)) {
+                    $count = $count + 0.5;
+                }
+                if (((strtotime($array['end_datetime']) - strtotime($array['start_datetime'])) / 3600 - 1.5) >= 8) {
+                    $count = $count + 1;
+                }
+
+                $people[(int)$member['code'] - 1] = $count;
+            }
+        }
+
+    }
+    $arrayAllMonth[] = $people;
+}
+
+$month1 = $arrayAllMonth[0];
+$month2 = $arrayAllMonth[1];
+$month3 = $arrayAllMonth[2];
+$month4 = $arrayAllMonth[3];
+$month5 = $arrayAllMonth[4];
+$month6 = $arrayAllMonth[5];
+$month7 = $arrayAllMonth[6];
+$month8 = $arrayAllMonth[7];
+$month9 = $arrayAllMonth[8];
+$month10 = $arrayAllMonth[9];
+$month11 = $arrayAllMonth[10];
+$month12 = $arrayAllMonth[11];
+
+$money = [];
+
+for ($i = 0; $i < count($listMemberFullTime); $i++) {
+    if (!isset($month1[$i])) {
+        $month1[$i] = 0;
+    }
+    if (!isset($month2[$i])) {
+        $month2[$i] = 0;
+    }
+    if (!isset($month3[$i])) {
+        $month3[$i] = 0;
+    }
+    if (!isset($month4[$i])) {
+        $month4[$i] = 0;
+    }
+    if (!isset($month5[$i])) {
+        $month5[$i] = 0;
+    }
+    if (!isset($month6[$i])) {
+        $month6[$i] = 0;
+    }
+    if (!isset($month7[$i])) {
+        $month7[$i] = 0;
+    }
+    if (!isset($month8[$i])) {
+        $month8[$i] = 0;
+    }
+    if (!isset($month9[$i])) {
+        $month9[$i] = 0;
+    }
+    if (!isset($month10[$i])) {
+        $month10[$i] = 0;
+    }
+    if (!isset($month11[$i])) {
+        $month11[$i] = 0;
+    }
+    if (!isset($month12[$i])) {
+        $month12[$i] = 0;
+    }
+
+    $real_money =
+        $listMemberFullTime[$i]['salary'] / $workday1 * $month1[$i]
+        + $listMemberFullTime[$i]['salary'] / $workday2 * $month2[$i]
+        + $listMemberFullTime[$i]['salary'] / $workday3 * $month3[$i]
+        + $listMemberFullTime[$i]['salary'] / $workday4 * $month4[$i]
+        + $listMemberFullTime[$i]['salary'] / $workday5 * $month5[$i]
+        + $listMemberFullTime[$i]['salary'] / $workday6 * $month6[$i]
+        + $listMemberFullTime[$i]['salary'] / $workday7 * $month7[$i]
+        + $listMemberFullTime[$i]['salary'] / $workday8 * $month8[$i]
+        + $listMemberFullTime[$i]['salary'] / $workday9 * $month9[$i]
+        + $listMemberFullTime[$i]['salary'] / $workday10 * $month10[$i]
+        + $listMemberFullTime[$i]['salary'] / $workday11 * $month11[$i]
+        + $listMemberFullTime[$i]['salary'] / $workday12 * $month12[$i];
+
+    array_push($money, $real_money);
+}
+
+for ($i = 0; $i < count($listMemberFullTime); $i++) {
+    $listMemberFullTime[$i]['salary'] = $money[$i];
+}
+
+echo "<pre>";
+print_r($listMemberFullTime);
+print_r($arrayAllMonth);
