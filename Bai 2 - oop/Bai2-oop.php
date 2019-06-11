cf284128a957b3ebafb0080c13751ae068d2b42d
Duong Anh Quy <anhquygt102@gmail.com>
2019-06-11 13:44:57 +0700

    bai2 - oop

--- Bai 2 - oop/Bai2-oop.php
+++ Bai 2 - oop/Bai2-oop.php
@@ -1,40 +1,42 @@
 <?php
 
-class Language
-{
-
-    public function split_string($txt)
-    {
-        $arr = [];
-        $file = fopen($txt, "r");
-        while (feof($file) == false) {
-            $lg[] = fgets($file);
-        }
-        fclose($file);
-
-        foreach ($lg as $key => $value) {
-            $result = explode('=', $value);
-            $arr[$result[0]] = $result[1];
-        }
-        return $arr;
-    }
-
-    public static function html($vi, $eng)
-    {
-        if (isset($_POST['select'])) {
-            if ($_POST['select'] == '1') {
-                echo $vi;
-            }
-            if ($_POST['select'] == '2') {
-                echo $eng;
-            }
-        } else {
-            echo $eng;
-        }
-    }
-
-}
-
+//class Language
+//{
+//
+//    public function split_string($txt)
+//    {
+//        $arr = [];
+//        $file = fopen($txt, "r");
+//        while (feof($file) == false) {
+//            $lg[] = fgets($file);
+//        }
+//        fclose($file);
+//
+//        foreach ($lg as $key => $value) {
+//            $result = explode('="', $value);
+//            $outcome = explode('"', $result[1]);
+//            $arr[$result[0]] = $outcome[0];
+//        }
+//
+//        return $arr;
+//    }
+//
+//    public static function html($vi, $eng)
+//    {
+//        if (isset($_POST['select'])) {
+//            if ($_POST['select'] == '1') {
+//                echo $vi;
+//            }
+//            if ($_POST['select'] == '2') {
+//                echo $eng;
+//            }
+//        } else {
+//            echo $eng;
+//        }
+//    }
+//
+//}
+//
 //$arrVi = [];
 //$fileVi = fopen("vi.txt", "r");
 //while(feof($fileVi ) == false) {
@@ -59,4 +61,5 @@ class Language
 //    $arrEng[$result[0]] = $result[1];
 //}
 
-?>
+
+
